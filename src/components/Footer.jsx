94b0d350665bcd61487ded70de8a340a1d9d1511
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2025-01-05 17:32:53 +0100

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+    return <footer className="bg-red text-center p-10 flex flex-col gap-5">
+        <h1 className="text-2xl text-white font-semibold">Ksiądz Jerzy Popiełuszko</h1>
+        <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p className="text-white text-sm">Jakub Lagierski, Dawid Chaberski, Tobiasz Pietrzak</p>
+    </footer>
+}
