1b7f44776d1e660e5ac96c03c0884844fb97475d
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2025-01-05 16:41:09 +0100

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
+import { Navbar } from "./components/Navbar";
+import { Card } from "./components/Card";
 function App() {
+  const Data = [
+    {
+      id: 0,
+      titleText: "Ksiądz Jerzy Popiełuszko",
+      titleColor: "white",
+      descriptionText:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      descriptionColor: "white",
+      photoUrl: "#",
+      bgColor: "red",
+    },
+    {
+      id: 1,
+      titleText: "Komponent",
+      titleColor: "white",
+      descriptionText:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      descriptionColor: "white",
+      photoUrl: "#",
+      bgColor: "red",
+    },
+    {
+      id: 2,
+      titleText: "Komponent",
+      titleColor: "black",
+      descriptionText:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      descriptionColor: "black",
+      photoUrl: "#",
+      bgColor: "white",
+    },
+  ];
   return (
-    <div>
-      
-    </div>
-  )
+    <main>
+      <Navbar />
+      {Data.map((card) => (
+        <Card key={card.id} data={card} />
+      ))}
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return <nav className="bg-black font-semibold py-5 px-20 flex justify-between">
+        <h1 className="text-red">Ksiądz Jerzy Popiełuszko</h1>
+        <div className="flex gap-7 text-red font-thin">
+            <p>Mapa</p>
+            <p>Życiorys</p>
+        </div>
+    </nav>
+}
